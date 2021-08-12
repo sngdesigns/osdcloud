b282709fce728e64d27adc7469420503798f56cc
sngdesigns <sngdesigns@gmail.com>
2021-08-12 14:50:23 -0700

    added sysprep

--- Windows 10 20H2 Enterprise en-us.ps1
+++ Windows 10 20H2 Enterprise en-us.ps1
@@ -30,6 +30,17 @@ $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeplo
 $AuditUnattendXml = @'
 <?xml version="1.0" encoding="utf-8"?>
 <unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="specialize" wasPassProcessed="true">
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <RunSynchronous>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <Description>OSDCloud Specialize</Description>
+                    <Path>Powershell -ExecutionPolicy Bypass -Command Invoke-OSDSpecialize -Verbose</Path>
+                </RunSynchronousCommand>
+            </RunSynchronous>
+        </component>
+    </settings>
     <settings pass="oobeSystem">
         <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <Reseal>
@@ -40,25 +51,34 @@ $AuditUnattendXml = @'
     <settings pass="auditUser">
         <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <RunSynchronous>
-            
-            <RunSynchronousCommand wcm:action="add">
-            <Order>1</Order>
-            <Description>Setting PowerShell ExecutionPolicy</Description>
-            <Path>PowerShell -WindowStyle Hidden -Command "Set-ExecutionPolicy RemoteSigned -Force"</Path>
-            </RunSynchronousCommand>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <Description>Setting PowerShell ExecutionPolicy</Description>
+                    <Path>PowerShell -WindowStyle Hidden -Command "Set-ExecutionPolicy RemoteSigned -Force"</Path>
+                </RunSynchronousCommand>
 
-            <RunSynchronousCommand wcm:action="add">
-            <Order>2</Order>
-            <Description>Update OSD Module</Description>
-            <Path>PowerShell -Command "Install-Module OSD -Force"</Path>
-            </RunSynchronousCommand>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>2</Order>
+                    <Description>Update OSD Module</Description>
+                    <Path>PowerShell -Command "Install-Module OSD -Force"</Path>
+                </RunSynchronousCommand>
 
-            <RunSynchronousCommand wcm:action="add">
-            <Order>3</Order>
-            <Description>OOBEDeploy</Description>
-            <Path>PowerShell -Command "Start-OOBEDeploy -AddNetFX3 -UpdateDrivers -UpdateWindows"</Path>
-            </RunSynchronousCommand>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>3</Order>
+                    <Description>Update Windows</Description>
+                    <Path>PowerShell -Command "Start-OOBEDeploy -AddNetFX3 -UpdateDrivers -UpdateWindows"</Path>
+                </RunSynchronousCommand>
 
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>4</Order>
+                    <Description>Syspre</Description>
+                    <Path>PowerShell -Command "Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /reboot /quiet'"</Path>
+                </RunSynchronousCommand>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>5</Order>
+                    <Description>Restart Computer</Description>
+                    <Path>PowerShell -Command "Restart-Computer"</Path>
+                </RunSynchronousCommand>        
             </RunSynchronous>
         </component>
     </settings>
@@ -74,4 +94,4 @@ if (-NOT (Test-Path $PantherUnattendPath)) {
 $AuditUnattendPath = Join-Path $PantherUnattendPath 'Unattend.xml'
 $AuditUnattendXml | Out-File -FilePath $AuditUnattendPath -Encoding utf8
 Use-WindowsUnattend -Path 'C:\' -UnattendPath $AuditUnattendPath -Verbose
-#Restart-Computer
+Restart-Computer
