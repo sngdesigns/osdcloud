95980e5fdb471cbe379a14c56e30f22a74d2f273
sngdesigns <sngdesigns@gmail.com>
2021-08-12 12:28:40 -0700

    minor change

--- Windows 10 20H2 Enterprise en-us.ps1
+++ Windows 10 20H2 Enterprise en-us.ps1
@@ -23,29 +23,43 @@ If (!(Test-Path "C:\ProgramData\OSDeploy")) {
 }
 $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
 
-$UnattendXml = @'
-<?xml version='1.0' encoding='utf-8'?>
+#================================================
+#   PostOS
+#   Audit Mode OOBEDeploy
+#================================================
+$AuditUnattendXml = @'
+<?xml version="1.0" encoding="utf-8"?>
 <unattend xmlns="urn:schemas-microsoft-com:unattend">
-    <settings pass="specialize" wasPassProcessed="true">
-        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <RunSynchronous>
-                <RunSynchronousCommand wcm:action="add">
-                    <Order>1</Order>
-                    <Description>OSDCloud Specialize</Description>
-                    <Path>Powershell -ExecutionPolicy Bypass -Command Invoke-OSDSpecialize -Verbose</Path>
-                </RunSynchronousCommand>
-            </RunSynchronous>
-        </component>
-    </settings>
     <settings pass="oobeSystem">
         <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <FirstLogonCommands>
-                <SynchronousCommand wcm:action="add">
-                    <Order>1</Order>
-                    <Description>OSDCloud UpdateWindows</Description>
-                    <Path>Powershell -ExecutionPolicy Bypass -Command Start-OOBEDeploy -UpdateWindows -Verbose</Path>
-                </SynchronousCommand>
-            </FirstLogonCommands>
+            <Reseal>
+                <Mode>Audit</Mode>
+            </Reseal>
+        </component>
+    </settings>
+    <settings pass="auditUser">
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <RunSynchronous>
+            
+            <RunSynchronousCommand wcm:action="add">
+            <Order>1</Order>
+            <Description>Setting PowerShell ExecutionPolicy</Description>
+            <Path>PowerShell -WindowStyle Hidden -Command "Set-ExecutionPolicy RemoteSigned -Force"</Path>
+            </RunSynchronousCommand>
+
+            <RunSynchronousCommand wcm:action="add">
+            <Order>2</Order>
+            <Description>Update OSD Module</Description>
+            <Path>PowerShell -Command "Install-Module OSD -Force"</Path>
+            </RunSynchronousCommand>
+
+            <RunSynchronousCommand wcm:action="add">
+            <Order>3</Order>
+            <Description>OOBEDeploy</Description>
+            <Path>PowerShell -Command "Start-OOBEDeploy -AddNetFX3 -UpdateDrivers -UpdateWindows"</Path>
+            </RunSynchronousCommand>
+
+            </RunSynchronous>
         </component>
     </settings>
 </unattend>
@@ -57,7 +71,7 @@ $PantherUnattendPath = 'C:\Windows\Panther\Unattend'
 if (-NOT (Test-Path $PantherUnattendPath)) {
     New-Item -Path $PantherUnattendPath -ItemType Directory -Force | Out-Null
 }
-$PantherUnattendPath = Join-Path $PantherUnattendPath 'Unattend.xml'
-$UnattendXml | Out-File -FilePath $PantherUnattendPath -Encoding utf8
-Use-WindowsUnattend -Path 'C:\' -UnattendPath $PantherUnattendPath -Verbose
+$AuditUnattendPath = Join-Path $PantherUnattendPath 'Unattend.xml'
+$AuditUnattendXml | Out-File -FilePath $AuditUnattendPath -Encoding utf8
+Use-WindowsUnattend -Path 'C:\' -UnattendPath $AuditUnattendPath -Verbose
 #Restart-Computer
