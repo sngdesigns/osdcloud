b30221d40177c449c1c730ec15b5b4683b7ef4c5
sngdesigns <sngdesigns@gmail.com>
2021-08-12 10:28:48 -0700

    added oobesystem to unattended

--- Windows 10 20H2 Enterprise en-us.ps1
+++ Windows 10 20H2 Enterprise en-us.ps1
@@ -31,10 +31,21 @@ $UnattendXml = @'
             <RunSynchronous>
                 <RunSynchronousCommand wcm:action="add">
                     <Order>1</Order>
-                    <Description>OSDCloud UpdateWindows</Description>
-                    <Path>Powershell -ExecutionPolicy Bypass -Command Start-OOBEDeploy -UpdateWindows -Verbose</Path>
+                    <Description>OSDCloud Specialize</Description>
+                    <Path>Powershell -ExecutionPolicy Bypass -Command Invoke-OSDSpecialize -Verbose</Path>
                 </RunSynchronousCommand>
             </RunSynchronous>
+        </component>
+    </settings>
+    <settings pass="oobeSystem">
+        <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <FirstLogonCommands>
+                <SynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <Description>OSDCloud UpdateWindows</Description>
+                    <Path>Powershell -ExecutionPolicy Bypass -Command Start-OOBEDeploy -UpdateWindows -Verbose</Path>
+                </SynchronousCommand>
+            </FirstLogonCommands>
         </component>
     </settings>
 </unattend>
