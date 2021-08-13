7ea21acfc96038ed13ed47479f12ccbcc5e0801d
sngdesigns <sngdesigns@gmail.com>
2021-08-13 13:26:20 -0700

    added save mscatupdates

--- Windows 10 20H2 Enterprise en-us.ps1
+++ Windows 10 20H2 Enterprise en-us.ps1
@@ -1,42 +1,81 @@
 ##############################################
 # :: Set Configuration ::
 ##############################################
-$Param = @{
+$Params = @{
     OSBuild     = "20H2"
     OSEdition   = "Enterprise"
     Culture     = "en-us"
     SkipAutopilot = $true
     SkipODT     = $true
-    ZTI         = $true
 }
 
-Start-OSDCloud @Param
+Start-OSDCloud @Params -ZTI
 
-$Params = @{
-    UpdateWindows = $true
-}
-Start-OOBEDeploy @Params
 #================================================
 #   WinPE PostOS
-#   Set OOBEDeploy CMD.ps1
+#   Set Install-WindowsUpdate.ps1
 #================================================
 $SetCommand = @'
-@echo off
-:: Set the PowerShell Execution Policy
-PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
-:: Add PowerShell Scripts to the Path
-set path=%path%;C:\Program Files\WindowsPowerShell\Scripts
-:: Open and Minimize a PowerShell instance just in case
-start PowerShell -NoL -W Mi
-:: Install the latest OSD Module
-start "Install-Module OSD" /wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
-:: Start-OOBEDeploy
-:: There are multiple example lines. Make sure only one is uncommented
-:: The next line assumes that you have a configuration saved in C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json
-start "Start-OOBEDeploy" PowerShell -NoL -C Start-OOBEDeploy
-:: The next line assumes that you do not have a configuration saved in or want to ensure that these are applied
-start "Start-OOBEDeploy" PowerShell -NoL -C Start-OOBEDeploy -UpdateWindows
-exit
+$Location = "$env:SystemDrive\MSCatUpdates"
+$Updates = (Get-ChildItem $Location | Where-Object {$_.Extension -eq '.msu'} | Sort-Object {$_.LastWriteTime} )
+$Qty = $Updates.count
+
+if (!(Test-Path $env:systemroot\SysWOW64\wusa.exe)){
+    $Wus = "$env:systemroot\System32\wusa.exe"
+  }
+  else {
+    $Wus = "$env:systemroot\SysWOW64\wusa.exe"
+  }
+  
+foreach ($Update in $Updates)
+  {
+    Write-Information "Starting Update $Qty - `r`n$Update"
+    Start-Process -FilePath $Wus -ArgumentList ($Update.FullName, '/quiet', '/norestart') -Wait
+    Write-Information "Finished Update $Qty"
+  }  
 '@
-$SetCommand | Out-File -FilePath "C:\Windows\OOBEDeploy.cmd" -Encoding ascii -Force
-#Restart-Computer
+$SetCommand | Out-File -FilePath "C:\Windows\Install-Updates.ps1" -Encoding ascii -Force
+
+#================================================
+#   Download latest Windows update from Microsoft
+#================================================
+Save-MsUpCatUpdate -OS 'Windows 10' -Arch x64 -Build 20H2 -Category SSU -Latest -DestinationDirectory C:\MSCatUpdates
+Save-MsUpCatUpdate -OS 'Windows 10' -Arch x64 -Build 20H2 -Category DotNetCU -Latest -DestinationDirectory C:\MSCatUpdates
+Save-MsUpCatUpdate -OS 'Windows 10' -Arch x64 -Build 20H2 -Category LCU -Latest -DestinationDirectory C:\MSCatUpdates
+
+#================================================
+#   PostOS
+#================================================
+$UnattendXml = @'
+<?xml version='1.0' encoding='utf-8'?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="specialize" wasPassProcessed="true">
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <RunSynchronous>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <Description>OSDCloud Specialize</Description>
+                    <Path>Powershell -ExecutionPolicy Bypass -Command Invoke-OSDSpecialize -Verbose</Path>
+                </RunSynchronousCommand>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <Description>Install Windows Update</Description>
+                    <Path>Powershell -ExecutionPolicy Bypass -File 'C:\Windows\Install-Updates.ps1'</Path>
+                </RunSynchronousCommand>
+            </RunSynchronous>
+        </component>
+    </settings>
+</unattend>
+'@
+#================================================
+#   Set Unattend.xml
+#================================================
+$PantherUnattendPath = 'C:\Windows\Panther\Unattend'
+if (-NOT (Test-Path $PantherUnattendPath)) {
+    New-Item -Path $PantherUnattendPath -ItemType Directory -Force | Out-Null
+}
+$UnattendPath = Join-Path $PantherUnattendPath 'Unattend.xml'
+$UnattendXml | Out-File -FilePath $UnattendPath -Encoding utf8
+Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath -Verbose
+
+Restart-Computer
