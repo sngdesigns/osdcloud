37fa437f650f707ac7884f324a5c1b0dcc27fd79
sngdesigns <sngdesigns@gmail.com>
2021-08-13 14:11:39 -0700

    minor change

--- Windows 10 20H2 Enterprise en-us.ps1
+++ Windows 10 20H2 Enterprise en-us.ps1
@@ -67,6 +67,16 @@ $UnattendXml = @'
             </RunSynchronous>
         </component>
     </settings>
+    <settings pass="oobeSystem">
+        <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <FirstLogonCommands>
+                <SynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <CommandLine>Powershell -ExecutionPolicy Bypass -File C:\Windows\Install-Updates.ps1</CommandLine>
+                </SynchronousCommand>
+            </FirstLogonCommands>
+        </component>
+    </settings>    
 </unattend>
 '@
 #================================================
@@ -80,4 +90,4 @@ $UnattendPath = Join-Path $PantherUnattendPath 'Unattend.xml'
 $UnattendXml | Out-File -FilePath $UnattendPath -Encoding utf8
 Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath -Verbose
 
-Restart-Computer
+#Restart-Computer
