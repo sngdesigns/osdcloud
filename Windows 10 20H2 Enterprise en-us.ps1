cf90f878d7a6fa91325c561f775bfd984e631beb
sngdesigns <sngdesigns@gmail.com>
2021-08-23 12:18:10 -0700

    2021.08.23

--- Windows 10 20H2 Enterprise en-us.ps1
+++ Windows 10 20H2 Enterprise en-us.ps1
@@ -67,10 +67,6 @@ Function Install-MSUpdates{
 }
 
 Install-MSUpdates
-
-# Cleanup
-Remove-Item -Path C:\MSUpdates -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path C:\OSDCloud -Recurse -ErrorAction SilentlyContinue
 '@
 $SetCommand | Out-File -FilePath "C:\Windows\Install-Updates.ps1" -Encoding ascii -Force
 
@@ -100,7 +96,17 @@ $UnattendXml = @'
                     <Order>2</Order>
                     <Description>Install Windows Update</Description>
                     <Path>Powershell -ExecutionPolicy Bypass -File C:\Windows\Install-Updates.ps1</Path>
-                </RunSynchronousCommand>              
+                </RunSynchronousCommand>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>3</Order>
+                    <Description>Remove Windows Update Files</Description>
+                    <Path>Powershell -ExecutionPolicy Bypass -Command Remove-Item -Path C:\MSUpdates -Recurse</Path>
+                </RunSynchronousCommand>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>4</Order>
+                    <Description>Remove OSDCloud Temp Files</Description>
+                    <Path>Powershell -ExecutionPolicy Bypass -Command Remove-Item -Path C:\OSDCloud -Recurse</Path>
+                </RunSynchronousCommand>                
             </RunSynchronous>
         </component>
     </settings>    
