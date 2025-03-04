ef022dcc586c12509393c526a1c74f0840f2a253
Ng, Steven <stng@informatica.com>
2025-03-04 10:11:40 -0800

    2025.03.04.03

--- Windows_11_23H2_Enterprise_en-us.ps1
+++ Windows_11_23H2_Enterprise_en-us.ps1
@@ -111,34 +111,9 @@ $UnattendXml = @'
             <RunSynchronous>
                 <RunSynchronousCommand wcm:action="add">
                     <Order>1</Order>
-                    <Description>OSDCloud Specialize</Description>
-                    <Path>#Powershell -ExecutionPolicy Bypass -Command Invoke-OSDSpecialize -Verbose</Path>
-                </RunSynchronousCommand>
-                <RunSynchronousCommand wcm:action="add">
-                    <Order>2</Order>
                     <Description>Install Windows Update</Description>
                     <Path>Powershell -ExecutionPolicy Bypass -File C:\Windows\Install-Updates.ps1</Path>
-                </RunSynchronousCommand>
-                <RunSynchronousCommand wcm:action="add">
-                    <Order>3</Order>
-                    <Description>Remove Windows Update Files</Description>
-                    <Path>Powershell -ExecutionPolicy Bypass -Command Remove-Item -Path C:\MSUpdates -Recurse</Path>
-                </RunSynchronousCommand>
-                <RunSynchronousCommand wcm:action="add">
-                    <Order>4</Order>
-                    <Description>Remove OSDCloud Temp Files</Description>
-                    <Path>#Powershell -ExecutionPolicy Bypass -Command Remove-Item -Path C:\OSDCloud -Recurse</Path>
-                </RunSynchronousCommand>
-                <RunSynchronousCommand wcm:action="add">
-                    <Order>5</Order>
-                    <Description>Remove Drivers Temp Files</Description>
-                    <Path>#Powershell -ExecutionPolicy Bypass -Command Remove-Item -Path C:\Drivers -Recurse</Path>
-                </RunSynchronousCommand>         
-                <RunSynchronousCommand wcm:action="add">
-                    <Order>6</Order>
-                    <Description>Remove Provisioning Package</Description>
-                    <Path>#Powershell -ExecutionPolicy Bypass -Command Remove-Item -Path C:\Recovery -Recurse</Path>
-                </RunSynchronousCommand>            
+                </RunSynchronousCommand>        
             </RunSynchronous>
         </component>
     </settings>    
