f5824f29f3a9a29edda7ec3c7e34acdcb787ec6a
Ng, Steven <stng@informatica.com>
2025-04-25 12:30:23 -0700

    2025.04.25.01

--- Windows_11_23H2_Enterprise_en-us.ps1
+++ Windows_11_23H2_Enterprise_en-us.ps1
@@ -92,8 +92,8 @@ New-Item "C:\MSUpdates\LCU" -ItemType Directory -Force
 # Write-Host "Downloading Latest Cumulative Update for Windows 11 23H2 - Dec 10, 2024"
 # curl.exe -L -o "C:\MSupdates\LCU\Windows11-23H2-LCU.msu" "https://catalog.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/d2584a30-89ea-4236-af04-2585566deaa6/public/windows11.0-kb5048685-x64_f1967f623976c41d20deab623317c4855e9d111a.msu"
 
-Write-Host "Downloading Latest Cumulative Update for Windows 11 23H2 - Mar 11, 2025"
-curl.exe -L -o "C:\MSupdates\LCU\Windows11-23H2-LCU.msu" "https://catalog.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/cf6a62b9-4c7f-4703-aeb7-9ef0df5f29f1/public/windows11.0-kb5053602-x64_c1dc9c521f329ff91d92ab59713cb0f01959fa4e.msu"
+Write-Host "Downloading Latest Cumulative Update for Windows 11 23H2 - April 8, 2025"
+curl.exe -L -o "C:\MSupdates\LCU\Windows11-23H2-LCU.msu" "https://catalog.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/e7260780-684e-4b11-bc36-475b34268a6d/public/windows11.0-kb5055528-x64_ad405ca048652f0197920698a0aa4b9fcd13da43.msu"
 
 # Use old unattended method instead of Provisioning ppkg to install drivers
 Set-OSDCloudUnattendSpecialize
